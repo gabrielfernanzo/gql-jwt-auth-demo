46c8d229dd84bcaa5d74135d95198ff8dc4cbd0e
Gabriel Fernandes <gabriel.work076@gmail.com>
2022-03-18 16:24:19 -0300

    feat(users): type-defs

--- src/type-defs/index.js
+++ src/type-defs/index.js
@@ -1,7 +1,40 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  input UserUniqueWhereInput {
+    id: String
+    email: String
+  }
+
+  input UserCreateInput {
+    name: String!
+    email: String!
+    password: String!
+    confirmPassword: String!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
   type Query {
     hello(name: String): String!
+    users(limt: Int, offset: Int): [User!]!
+    user(where: UserUniqueWhereInput!): User
+    me: User
+  }
+
+  type Mutation {
+    register(input: UserCreateInput!): User
+    login(input: LoginInput!): User
   }
 `;
